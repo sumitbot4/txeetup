6ae2e15e9af6ded47e2db1d7836cf3bb29f36a53
sumitbot4 <870.sumit123@gmail.com>
2025-08-19 16:50:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -580,7 +580,7 @@ async def cmd(client, callback_query):
     ])
     await callback_query.message.edit_media(
     InputMediaPhoto(
-      media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+      media="https://positivethinking.tech/wp-content/uploads/2023/09/Blog_BOT-Model_Featured-Image-1-800x450.png",
       caption=caption
     ),
     reply_markup=keyboard
@@ -619,7 +619,7 @@ async def help_button(client, callback_query):
     
   await callback_query.message.edit_media(
     InputMediaPhoto(
-      media="https://tinypic.host/images/2025/07/14/file_00000000fc2461fbbdd6bc500cecbff8_conversation_id6874702c-9760-800e-b0bf-8e0bcf8a3833message_id964012ce-7ef5-4ad4-88e0-1c41ed240c03-1-1.jpg",
+      media="https://positivethinking.tech/wp-content/uploads/2023/09/Blog_BOT-Model_Featured-Image-1-800x450.png",
       caption=caption
     ),
     reply_markup=keyboard
